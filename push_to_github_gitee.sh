1fe148450f86a940bea7048e759612961e42fd0f
woo-rookie <1572914362@qq.com>
2021-03-12 11:49:18 +0800

    2021年 03月 12日 星期五 11:49:18 CST

--- push_to_github_gitee.sh
+++ push_to_github_gitee.sh
@@ -43,8 +43,8 @@ git add .
 
 git commit -m "$(date)"
 
-git pull gitee master
-git pull github master
+# git pull gitee master
+# git pull github master
 
 git push gitee master
 git push github master
